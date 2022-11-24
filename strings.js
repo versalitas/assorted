7f5cc8389fc50559be482c9f92beb15696908186
versalitas <100954079+versalitas@users.noreply.github.com>
2022-11-24 14:53:54 +0100

    Update strings.js

--- strings.js
+++ strings.js
@@ -3,6 +3,19 @@
 
 const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('');
 
+//reverse a string
+
+const reverseStr =  => (str){
+  return str.split('').reverse().join('');  
+}
+
+
+const reverseString = (str) => {
+  let res = '';
+  for (let i = str.length - 1; i >= 0; i--)
+    res += str[i];
+  return res;
+}
 
 //parseInt takes a string and converts to integer (from mosh)
 
