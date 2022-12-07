5a66e7a359d250e877695a71ce596e743cbc3003
versalitas <100954079+versalitas@users.noreply.github.com>
2022-12-07 19:23:58 +0100

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -50,3 +50,13 @@ someArray.forEach(el => {if(some logic){console.log(el));
 //returns filtered array
 const newArray = someArray.filter(el => el.propiedad === 'input´);
 
+// delete first occurence of smallest value
+  const removeSmallest = (numbers) => {
+  //returns a copy starting from 0
+  numbers = numbers.slice(0);
+  const min = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(min), 1);
+  return numbers;
+}
+                                  
+
